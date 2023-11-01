2472480a54bdfe7ed2f576091502b4d130715969
Antonio S <asanchz85@gmail.com>
2023-11-01 22:26:23 +0100

    ending with logic

--- src/components/Wheel-spin.jsx
+++ src/components/Wheel-spin.jsx
@@ -1,17 +1,20 @@
 import "./Wheel-spin.css";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 const WheelSpin = () => {
+  const initalPosition = 345;
+  const wheelTransition = useRef(null);
   const [spinning, setSpinning] = useState(false);
   const [result, setResult] = useState(null);
+  const [angle, setAngle] = useState(initalPosition);
 
   const questions = [
-    "Question 1",
-    "Question 2",
-    "Question 3",
-    "Question 4",
-    "Question 5",
-    "Question 6",
+    "Beginner",
+    "Pre-Inter",
+    "Intermediate",
+    "Upper-Inter",
+    "Advanced",
+    "Proficient",
   ];
 
   const startSpin = () => {
@@ -19,31 +22,61 @@ const WheelSpin = () => {
 
     setSpinning(true);
 
-    // Simulate a random rotation duration (between 3 and 5 seconds)
-    const rotationDuration = Math.floor(Math.random() * 3 + 3) * 1000;
-    const rotationAngle = Math.floor(Math.random() * 360); // Random initial angle
+    const rotationAngle = Math.floor(Math.random() * 360) * 10; // Random initial angle
 
-    const wheel = document.querySelector(".wheel");
-    wheel.style.transition = `transform ${
-      rotationDuration / 1000
-    }s ease-in-out`;
-    wheel.style.transform = `rotate(${rotationAngle + 360 * 10}deg)`; // ??Rotate 10 times and then to the result
+    setAngle(rotationAngle + angle);
 
     setTimeout(() => {
-      const wheelAngle = rotationAngle % 360;
-      const segmentAngle = 360 / 6; // Six segments
-      const resultIndex = Math.floor(wheelAngle / segmentAngle);
-      setResult(resultIndex);
-
       setSpinning(false);
-    }, rotationDuration);
+    }, 3000);
   };
+
+  const handleTransition = () => {
+    const wheelPosition = Number(
+      wheelTransition.current.style.transform
+        .split("rotate(")[1]
+        .split("deg)")[0]
+    );
+    console.log(wheelPosition);
+    
+    let evaluatedAngle = (wheelPosition % 360) - initalPosition;
+    if (evaluatedAngle < 0) evaluatedAngle *= -1;
+
+    if (
+      (evaluatedAngle >= 316 && evaluatedAngle <= 360) ||
+      (evaluatedAngle >= 0 && evaluatedAngle <= 15)
+    ) {
+      setResult(0);
+    } else if (evaluatedAngle >= 16 && evaluatedAngle <= 75) {
+      setResult(1);
+    } else if (evaluatedAngle >= 76 && evaluatedAngle <= 135) {
+      setResult(2);
+    } else if (evaluatedAngle >= 136 && evaluatedAngle <= 195) {
+      setResult(3);
+    } else if (evaluatedAngle >= 196 && evaluatedAngle <= 255) {
+      setResult(4);
+    } else if (evaluatedAngle >= 256 && evaluatedAngle <= 315) {
+      setResult(5);
+    }
+  };
+
+  //?? fast the wheel -- pending
 
   return (
     <>
       <div className="wheel-spin">
-        <div className="spinBtn" onClick={startSpin}>spin</div>
-        <div className={`wheel ${spinning ? "spin" : ""}`}>
+        <div className="spinBtn" onClick={startSpin}>
+          spin
+        </div>
+        <div
+          className={`wheel`}
+          style={{
+            transform: `rotate(${angle}deg)`,
+            transition: `transform 3s ease-out`,
+          }}
+          ref={wheelTransition}
+          onTransitionEnd={handleTransition}
+        >
           {questions.map((question, index) => (
             <div
               key={index}
@@ -53,7 +86,9 @@ const WheelSpin = () => {
                 "--clr": `hsl(${(index * 360) / questions.length}, 100%, 50%)`,
               }}
             >
-              <span className={result === index ? "selected" : ""}>{question}</span>
+              <span className={result === index ? "selected" : ""}>
+                {question}
+              </span>
             </div>
           ))}
         </div>
@@ -63,7 +98,7 @@ const WheelSpin = () => {
           {spinning ? "Spinning..." : "Spin the Wheel"}
         </button>
         {result !== null && (
-          <p>
+          <p style={{ color: "whitesmoke" }}>
             Result: {questions[result]} (Segment {result + 1})
           </p>
         )}
